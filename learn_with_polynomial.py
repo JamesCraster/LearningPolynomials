70e66d4a4f944f680ee719d3d649092148d72ceb
JamesCraster <jamesvcraster@gmail.com>
2023-03-11 20:09:02 +0000

    added learning curves and plotted coefficient size

--- learn_with_polynomial.py
+++ learn_with_polynomial.py
@@ -4,11 +4,15 @@ import random
 import matplotlib.pyplot as plt
 
 # usually noise standard deviation is set to a small value, e.g 0.1
-noise_sd = 0.4
+noise_sd = 0.2
+
+def pattern(input_value):
+    return math.sin(input_value * 2 * math.pi)
+
 def generate_training_value():
     input_value = random.uniform(0,2)
     # our data has an underlying pattern of sin(2 * pi * x), with a small gaussian noise
-    return (input_value, math.sin(2 * math.pi * input_value) + np.random.normal(0.1,noise_sd,1)[0])
+    return (input_value, pattern(input_value) + np.random.normal(0.1,noise_sd,1)[0])
 
 # sample a finite number of points for our training set
 training_set_size = 20
@@ -30,7 +34,7 @@ for ax in axs:
 
 # render the underlying sin(2 * pi * x), so we can visualise how close the learning model is to the underlying pattern (as opposed to the noise)
 pattern_inputs = np.arange(0,2,0.01)
-pattern_values = np.sin(pattern_inputs * 2 * math.pi)
+pattern_values = list(map(pattern, pattern_inputs))
 for ax in axs:
     ax.plot(pattern_inputs, pattern_values)
 
@@ -39,9 +43,10 @@ model_errors = []
 validation_errors = []
 
 
-#def root_mean_square(inputs, values):
-#    return sum([(prediction_model(training_inputs[i]) - training_values[i]) ** 2 for i in range(0,len(training_inputs))])
+def root_mean_square(prediction_model, inputs, values):
+    return math.sqrt(sum([(prediction_model(inputs[i]) - values[i]) ** 2 for i in range(0,len(inputs))]) / len(inputs))
 
+coefficient_sizes = []
 
 for size_increment in range(0, max_model_size - min_model_size):
     model_size = min_model_size + size_increment
@@ -56,6 +61,9 @@ for size_increment in range(0, max_model_size - min_model_size):
 
     optimal_parameters = np.matmul(A_inv, T)
 
+    # plot the increasing coefficient size
+    coefficient_sizes.append((model_size, np.amax(optimal_parameters)))
+    
     # at this point, we construct our polynomial model, using the optimal parameters that minimise the least-squares error function
     def prediction_model(input_value):
         output = 0
@@ -63,25 +71,35 @@ for size_increment in range(0, max_model_size - min_model_size):
             output += optimal_parameters[i] * input_value ** i
         return output
 
-    prediction_values = list(map(prediction_model, pattern_inputs))
+    # make plot of predictions over entire range
+    pattern_prediction_values = list(map(prediction_model, pattern_inputs))
     # plot our polynomial model
-    model_plot, = axs[size_increment].plot(pattern_inputs, prediction_values)
+    model_plot, = axs[size_increment].plot(pattern_inputs, pattern_prediction_values)
     axs[size_increment].legend(handles=[model_plot], labels=[f"M = {model_size}"])
 
-    model_errors.append((model_size, sum([(prediction_model(training_inputs[i]) - training_values[i]) ** 2 for i in range(0,len(training_inputs))])))
-    #validation_errors.append()
+    # keep track of the training error
+    model_errors.append((model_size, root_mean_square(prediction_model, training_inputs, training_values)))
+    
+    # generate the validation error
+    validation_errors.append((model_size, root_mean_square(prediction_model, validation_inputs, validation_values)))
+
 for ax in axs:
     ax.set_ylim(bottom=-2, top=2)
 
 # stop the suptitle overlapping with the subplots
 fig.subplots_adjust(top=0.95)
-fig.suptitle('Learning sin(2πx) using a M-order polynomial model')
+fig.suptitle('Learning sin(2πx) using a M-order polynomial model (no regularisation)')
 plt.show()
 
-
-plt.plot(*list(zip(*model_errors)))
+plt.title('Learning curves')
+plt.plot(*list(zip(*model_errors)), '-o', label='Training error')
+plt.plot(*list(zip(*validation_errors)), '-o', label='Validation error')
+plt.legend()
 plt.show()
 
+plt.title('Largest coefficient as model size increases')
+plt.plot(*list(zip(*coefficient_sizes)), '-o')
+plt.show()
 
 
 
